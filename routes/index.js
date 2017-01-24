3600f055921dac1c878447c8a91b35b4ff207704
deverseli800 <cesar.devers@gmail.com>
2017-01-23 21:33:39 -0500

    more team page

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Bowery Real Estate Systems' });
 });
 
+router.get('/team', function(req, res, next) {
+  res.render('team', { title: 'Our Team - Bowery Real Estate Systems' });
+});
+
 var nodemailer = require('nodemailer');
 
 // router.post('/contact', function (req, res) {
